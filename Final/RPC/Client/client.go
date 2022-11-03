1e7d49b3627c432c3a4ff916887b2e964c0c109c
Victor Ximenes <54337351+vxima@users.noreply.github.com>
2022-11-03 12:17:07 -0300

    Conexao feita no rpc

--- Final/RPC/Client/client.go
+++ Final/RPC/Client/client.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"net/rpc"
 	"sync"
 	"time"
 )
 
 var wg sync.WaitGroup
 
+type Args struct {
+	ID   int
+	time time.Duration
+}
 type Client struct {
 	ID   int
 	time time.Duration
@@ -32,8 +38,24 @@ func client_time() time.Duration {
 }
 func main() {
 	fmt.Println("Clients start coming")
-	qntdClients := 5
-	for i := 1; i <= qntdClients; i++ {
+	//qntdClients := 1
+	/*for i := 1; i <= qntdClients; i++ {
 		client := NewClient(i)
+	}*/
+	conn, err := rpc.DialHTTP("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("Connectiong:", err)
 	}
+	fmt.Println("Connection Successfully")
+	//Synchronus call
+	client := NewClient(1)
+	args := &Args{client.ID, client.time}
+	var reply int
+	// call
+	err = conn.Call("Client.GiveClientID", args, &reply)
+	if err != nil {
+		log.Fatal("ID error:", err)
+	}
+	fmt.Printf("Client info: {%d ,%d} , received= %d\n", args.ID, args.time, reply)
+
 }
--- Final/RPC/Server/server.go
+++ Final/RPC/Server/server.go
@@ -4,27 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
 	"time"
 )
 
+type Args struct {
+	ID   int
+	time time.Duration
+}
 type Client struct {
 	ID   int
 	time time.Duration
 }
 
+func (c *Client) GiveClientID(args *Args, reply *int) error {
+	*reply = args.ID
+	return nil
+}
 func registerClient(server *rpc.Server, c Client) {
-	server.RegisterName("Client", c)
+	rpc.Register(c)
 }
 
 func main() {
-	//client := new(Client)
-	server := rpc.NewServer()
-	//registerClient(server, client)
+	client := new(Client)
+	rpc.Register(client)
+	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":8080")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
 	fmt.Println("Listening in port 8080")
-	server.Accept(l)
+	http.Serve(l, nil)
 }
